ef41af260c8fa963e5afe4b063cb6b72bfaebdf5
re1ta <ernest.ibatov@mail.ru>
2023-06-13 15:18:51 +0500

    123

--- src/main/java/Plasma/Dynamics/repos/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package Plasma.Dynamics.repos;
-
-import Plasma.Dynamics.Dto.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Integer> {
-    User findByUsername(String username);
-}
